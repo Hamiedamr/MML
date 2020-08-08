e114782385559cf54af5266df421d6f3d4da8b4b
Ahmed-Salah6011 <54995214+Ahmed-Salah6011@users.noreply.github.com>
2020-08-08 12:34:03 +0200

    added encoder file

--- tansik_app.py
+++ tansik_app.py
@@ -5,6 +5,25 @@ from flask import Flask
 from flask import render_template,request
 app = Flask(__name__)
 
+def top_ten(y_proba):
+  y=y_proba[0]
+
+  m=[]
+  counter=0
+  while (counter<10):
+      mx_class=y.argmax()
+      counter+=1
+      m.append(mx_class)
+      y=np.delete(y,mx_class,0)
+  
+  return m
+  
+def predict(model,encoder,data):
+    y_proba = model.predict_proba([[float(data)]])
+    result=encoder.inverse_transform(top_ten(y_proba))
+    return result
+
+
 @app.route('/')
 def home():
     return render_template('public/tansik.html')
@@ -15,6 +34,7 @@ def tansiky():
     elif request.method == "POST":
         data = request.form['grade']
         model = joblib.load('thanwy_model.sav')
-        result = model.predict_proba([[float(data)]])
+        encoder = joblib.load('encoder.sav')
+        result=predict(model,encoder,float(data))
         return '<h1>'+str(result)+'</h1>'
     
